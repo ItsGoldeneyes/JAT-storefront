31536ff54fa3de58679168cc49db8531943ba96d
Thao Nguyen <thao2.nguyen@ryerson.ca>
2025-03-03 12:52:07 -0500

    adding req check for create_trip

--- Assignments/Assignment 1/Backend/create_trip.php
+++ Assignments/Assignment 1/Backend/create_trip.php
@@ -8,6 +8,11 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 include 'connect.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $data = json_decode(file_get_contents("php://input"));
 
 if (!$data) {
